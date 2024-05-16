6eebaf2544faf599d6b969aab05127d2bed65464
kasimir039 <kasimir39100@gmail.com>
2024-05-16 23:51:13 +0300

    .

---
+++ src/AreaOfCircle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AreaOfCircle
+{
+    public static void main(String[] args)
+    {
+        int r;
+        double area, pi= 3.14 ,alpha,circumference ;
+
+        System.out.println("Enter the radius value:");
+        Scanner input = new Scanner(System.in);
+
+        r = input.nextInt();
+
+        System.out.println("Enter the size of the central angle:");
+        alpha = input.nextDouble();
+
+        area = (pi * (r * r) * alpha) / 360;
+
+        System.out.println("Area of circle segment:" + area);
+
+
+    }
+}
