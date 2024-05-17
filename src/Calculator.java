1c9a1f87cb873c624a6b9181d0f773c0872ca644
kasimir039 <kasimir39100@gmail.com>
2024-05-17 16:29:56 +0300

    .

---
+++ src/Calculator.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        int n1,n2, select;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the first number");
+        n1 = input.nextInt();
+
+        System.out.println("Enter the second number");
+        n2 = input.nextInt();
+
+        System.out.println("Select action \n1)Addition\n2)Subtraction\n3)Multiplication\n4)Division");
+        select = input.nextInt();
+
+        switch (select)
+        {
+            case 1:
+                System.out.println("Result: n1 + n2 = " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Result: n1 - n2 = " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Result: n1 * n2 = " + (n1 * n2));
+                break;
+            case 4:
+                System.out.println("Result: n1 / n2 = " + (n1 / n2));
+                break;
+
+        }
+    }
+}
