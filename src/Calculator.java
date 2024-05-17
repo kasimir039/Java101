f90acad1c8d598ff852338861052c29d75c170ac
kasimir039 <kasimir39100@gmail.com>
2024-05-17 16:34:55 +0300

    .

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator
     public static void main(String[] args)
     {
         int n1,n2, select;
+        boolean flag = true;
 
         Scanner input = new Scanner(System.in);
 
@@ -17,21 +18,23 @@ public class Calculator
         System.out.println("Select action \n1)Addition\n2)Subtraction\n3)Multiplication\n4)Division");
         select = input.nextInt();
 
-        switch (select)
-        {
-            case 1:
-                System.out.println("Result: n1 + n2 = " + (n1 + n2));
-                break;
-            case 2:
-                System.out.println("Result: n1 - n2 = " + (n1 - n2));
-                break;
-            case 3:
-                System.out.println("Result: n1 * n2 = " + (n1 * n2));
-                break;
-            case 4:
-                System.out.println("Result: n1 / n2 = " + (n1 / n2));
-                break;
 
-        }
+      switch (select)
+      {
+          case 1:
+              System.out.println("Result: n1 + n2 = " + (n1 + n2));
+              break;
+          case 2:
+              System.out.println("Result: n1 - n2 = " + (n1 - n2));
+              break;
+          case 3:
+              System.out.println("Result: n1 * n2 = " + (n1 * n2));
+              break;
+          case 4:
+              System.out.println("Result: n1 / n2 = " + (n1 / n2));
+              break;
+          default:
+              System.out.println("Wrong choice");
+      }
     }
 }
