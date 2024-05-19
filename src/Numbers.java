386a4ae41d6f32dc045676af13ff29a5c04bd0ee
kasimir039 <kasimir39100@gmail.com>
2024-05-19 18:57:22 +0300

    .

---
+++ src/Numbers.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+/*Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam
+bölünen sayıların ortalamasını hesaplayan programı yazınız.*/
+
+class Numbers {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        int i = 0;
+        int total = 0;
+        int count = 0;
+
+        while(i <= n)
+        {
+            if((i % 3 == 0) && (i % 4 == 0))
+            {
+                total += i;
+                count++;
+            }
+            i++;
+
+        }
+
+        if(count > 0)
+        {
+            double average = (double) total  / count;
+            System.out.println("Average: " + average);
+        }
+    }
+}
+
