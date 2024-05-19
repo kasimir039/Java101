18be80c9d1c16fd65cbd35f2c5bcb22470a722e6
kasimir039 <kasimir39100@gmail.com>
2024-05-19 22:00:55 +0300

    .

--- src/Numbers2.java
+++ src/Numbers2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden
 çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.*/
 
@@ -7,8 +9,29 @@ public class Numbers2
     public static void main(String[] args)
     {
 
+        Scanner input = new Scanner(System.in);
 
 
+
+        int total = 0;
+
+        int n;
+
+        do {
+            n = input.nextInt();
+
+            if(n % 2 == 0  && n % 4 == 0)
+            {
+                total += n;
+            }
+
+        }while (!(n % 2 == 1));
+
+        System.out.println("Total: " + total);
 
     }
+
+
+
+
 }
