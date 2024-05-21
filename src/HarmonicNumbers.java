94192338563eda525492d8777a1fe7ab70f5dc2c
kasimir039 <kasimir39100@gmail.com>
2024-05-21 23:33:30 +0300

    .

---
+++ src/HarmonicNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class HarmonicNumbers {
+
+
+    public static void main(String[] args) {
+
+        double result = 0.0;
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("N: ");
+        n = input.nextInt();
+
+        for(double i = 1; i <= n;i++)
+        {
+            result += (1/i);
+
+        }
+        System.out.println("result: " + result);
+
+    }
+}
+
+
+
