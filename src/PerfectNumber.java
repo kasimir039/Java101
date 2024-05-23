151e689f518ec3f5026878aa0ec50c845cdf6261
kasimir039 <kasimir39100@gmail.com>
2024-05-23 13:56:10 +0300

    .

---
+++ src/PerfectNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PerfectNumber
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n1: " );
+        int num = input.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        if(sum == num)
+        {
+            System.out.println(num + " mükemmel bir sayıdır.");
+        }
+        else {
+            System.out.println(num + " mükemmel bir sayı değildir.");
+        }
+    }
+}
