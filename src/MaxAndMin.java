d6f740e8b2020c06981e5c0ebedcad311faaf8d2
kasimir039 <kasimir39100@gmail.com>
2024-05-23 12:42:02 +0300

    .

---
+++ src/MaxAndMin.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MaxAndMin {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many numbers do you want to enter?: ");
+        int n = input.nextInt();
+
+        int number;
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print(i + ".number: ");
+            number = input.nextInt();
+
+            if (number > max)
+                max = number;
+
+            if (number < min)
+                min = number;
+
+        }
+
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+
+    }
+}
