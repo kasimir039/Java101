79e74c8fedc56bc6a480bac5f66c4ccb83186f9f
kasimir039 <kasimir39100@gmail.com>
2024-05-26 16:09:00 +0300

    .

---
+++ src/Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fibonacci
+{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        long fib = 1, prev = 0;
+
+        for (int i = 1; i < n; i++) {
+            long temp = fib;
+            fib += prev;
+            prev = temp;
+            System.out.println(fib);
+
+        }
+
+    }
+}
