3ee3a11225460c1fb678ab85e25bd6f0f25c961c
kasimir039 <kasimir39100@gmail.com>
2024-05-31 00:47:06 +0300

    .

---
+++ SalaryCalculator/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        EmployeeManagement e = new EmployeeManagement("Ahmet",2000,45,1985);
+
+        String string = e.toString();
+
+        System.out.println(string);
+
+
+    }
+}
