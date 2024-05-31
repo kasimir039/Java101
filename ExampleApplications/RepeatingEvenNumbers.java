289e7dae23b2a27ecd372f1c0e607e09a6d3eddd
kasimir039 <kasimir39100@gmail.com>
2024-06-01 00:24:52 +0300

    .

---
+++ ExampleApplications/RepeatingEvenNumbers.java
@@ -0,0 +1,43 @@
+public class RepeatingEvenNumbers {
+
+    static boolean isFind(int[] arr,int value)
+    {
+        for(int i : arr)
+        {
+            if(i == value)
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] list = {2,2,4,4,8,8,10,21,1,33,9,1};
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+
+        for(int i = 0; i < list.length;i++)
+        {
+            for(int j = 0; j < list.length;j++)
+            {
+                if((i!=j) && (list[i] == list[j] && list[i] % 2 == 0) ) // && list[j] % 2 == 0
+                {
+                    if(!isFind(duplicate,list[i]))
+                    {
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+
+        for(int value: duplicate)
+        {
+            if(value != 0)
+            {
+                System.out.println(value);
+            }
+        }
+
+    }
+}
